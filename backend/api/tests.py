3536a5aa5eb0018ad48672f2ea877f26978d8571
Pavel Okhrim <133741770+d1g-1t@users.noreply.github.com>
2023-12-25 21:12:46 +0300

    tests.py fixed.

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from django.test import Client, TestCase
 from api import models
 
+
 class TaskiAPITestCase(TestCase):
     def setUp(self):
         self.guest_client = Client()
