6bc5d7adf7235afbd3ea55b4783a87b918efad11
Pavel Okhrim <133741770+d1g-1t@users.noreply.github.com>
2023-12-25 21:08:06 +0300

    tests.py fixed.

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,10 +1,7 @@
 # backend/api/tests.py
 from http import HTTPStatus
-
 from django.test import Client, TestCase
-
 from api import models
-
 
 class TaskiAPITestCase(TestCase):
     def setUp(self):
